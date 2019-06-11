b7985638d3ef6ac8e8ccf4fd5b1ea5d3236c570a
edwardsun007 <edwardsun007@gmail.com>
2019-06-11 14:43:08 -0700

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core"; // angular feature
 
-import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
+  imports: [FormsModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  //selector: 'app-server',  // typescript-selector
+  selector: ".app-servers", // select by class
+  templateUrl: "./server.component.html",
+  styleUrls: ["./server.component.css"]
+})
+export class ServerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
